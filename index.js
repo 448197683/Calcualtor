15bfcf51968dcb093147ff83f8ea292591cc0c17
448197683 <448197683@qq.com>
2021-11-08 15:46:38 +0800

    study 11.8

--- index.js
+++ index.js
@@ -1,8 +1,65 @@
 const numBtns = document.querySelectorAll('.numBtn');
 const result = document.querySelector('#result');
+const opBtns = document.querySelectorAll('.opBtn');
+const equalBtn = document.querySelector('.equalBtn');
+const clearBtn = document.querySelector('.clearBtn');
+
+let firstNum = '';
+let firstDone = false;
+let secondNum = '';
+let operator = '';
+
+clearBtn.addEventListener('click', (e) => {
+  firstNum = '';
+  firstDone = false;
+  secondNum = '';
+  secondDone = false;
+  operator = '';
+  result.value = '0';
+});
+
+equalBtn.addEventListener('click', () => {
+  switch (operator) {
+    case '+':
+      return (result.value = Number(firstNum) + Number(secondNum));
+    case '-':
+      return (result.value = Number(firstNum) - Number(secondNum));
+    case '*':
+      return (result.value = Number(firstNum) * Number(secondNum));
+    case '/':
+      return (result.value = Number(firstNum) / Number(secondNum));
+    default:
+      return;
+  }
+});
+
+/* equalBtn.addEventListener('click', (e) => {
+  if (operator === '+') {
+    result.value = Number(firstNum) + Number(secondNum);
+  } else if (operator === '-') {
+    result.value = Number(firstNum) - Number(secondNum);
+  } else if (operator === '*') {
+    result.value = Number(firstNum) * Number(secondNum);
+  } else if (operator === '/') {
+    result.value = Number(firstNum) / Number(secondNum);
+  }
+}); */
+
+opBtns.forEach((opBtn) => {
+  opBtn.addEventListener('click', (e) => {
+    firstDone = true;
+    operator = opBtn.value;
+  });
+});
 
 numBtns.forEach((numBtn) => {
   numBtn.addEventListener('click', (e) => {
-    result.value = numBtn.value;
+    if (firstDone === false) {
+      firstNum = firstNum + numBtn.value;
+      result.value = firstNum;
+    } else {
+      secondNum = secondNum + numBtn.value;
+      result.value = secondNum;
+    }
   });
 });
